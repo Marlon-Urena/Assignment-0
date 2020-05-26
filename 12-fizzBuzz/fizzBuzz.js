d83383507ed21c1f069b6a1ca8ce6b0c4a3fca24
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-05-26 17:00:57 +0100

    Completed Question 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,19 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+    let length = end - start + 1, value=start-1;
+    let array = new Array(length);
+    for(let i = 0; i < length; i++) {
+        value++;
+        if(value % 3 === 0 && value % 5 === 0) {
+            array[i] = 'FizzBuzz';
+        }
+        else if (value % 3 === 0)
+            array[i] = "Fizz";
+        else if (value % 5 === 0)
+            array[i] = "Buzz";
+        else
+            array[i] = value;
+    }
+  return array;
 }
 
 // Do not edit this line;
