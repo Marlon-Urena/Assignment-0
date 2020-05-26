e2be99433be859717a2d2882eca5f91e943034c7
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-05-26 16:55:48 +0100

    Completed Question 1

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,15 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    var startPosition = 0,
+        lastSpace = title.lastIndexOf(' ') + 1;
+    title = title.charAt(0).toUpperCase() + title.slice(1);
+
+    while (startPosition !== lastSpace) {
+        startPosition = title.indexOf(' ', startPosition) + 1;
+        title = title.slice(0, startPosition) + title.charAt(startPosition).toUpperCase() + title.slice(startPosition + 1);
+    }
+    return title;
 }
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
+
