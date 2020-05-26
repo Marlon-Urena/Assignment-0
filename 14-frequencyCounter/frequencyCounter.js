bef73477740828480176f6684d0df17bc3fd989f
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-05-26 17:01:53 +0100

    Completed Question 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
 function frequencyCounter(word) {
-  // Insert code here;
+    let letterMap = new Map();
+    let letter;
+    let count;
+  for(let i = 0; i < word.length; i++){
+      letter = word.charAt(i);
+    if (letterMap.has(letter)) {
+        count = letterMap.get(letter);
+        letterMap.set(letter,count+1)
+    }
+    else
+        letterMap.set(letter,1);
+  }
+    return Object.fromEntries(letterMap);
 }
 
 // Do not edit this line;
