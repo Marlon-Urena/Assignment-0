70b62463660813b9ffd599e98ff9605b764df586
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-05-26 16:59:51 +0100

    Completed Question 10

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,28 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+     let count=0, position, endOfArray=nums.length;
+     position = nums.indexOf(start);
+     if(position===-1)
+         return 0;
+
+    while(position !== endOfArray) {
+        if (nums[position] === end) {
+            count++;
+            break;
+        }
+        count++;
+        position++;
+    }
+
+    return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
+
+// for (let i = position; i < nums.length; i++) {
+//     if (nums[i] === end) {
+//         count++;
+//         break;
+//     }
+//     count++;
+// }
