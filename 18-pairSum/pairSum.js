41fbe24ce2213dc2aa03edeed1b6c04629670c0f
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-05-26 17:03:08 +0100

    Completed Question 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
-  // Insert code here;
+    let valueMap = new Map();
+    let value = 0;
+    if(nums.length <= 1){
+        throw "Array must be of length more than 1";
+    }
+    for (let i = 0 ; i < nums.length; i++) {
+       value = target - nums[i];
+        if(valueMap.has(value)) {
+            return true;
+        }
+        valueMap.set(nums[i],target);
+    }
+    return false;
 }
 
 // Do not edit this line;
