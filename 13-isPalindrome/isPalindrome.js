7e20508af61ebaa1ed81b78e8ef9cc6faca14234
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-05-26 17:01:17 +0100

    Completed Question 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
-  // Insert code here;
+    let iterations = Math.ceil(word.length/2);
+    let start = 0, end = word.length - 1;
+    for (let i = 0; i < iterations; i++) {
+        if (word[start] !== word[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 }
-
 // Do not edit this line;
 module.exports = isPalindrome;
